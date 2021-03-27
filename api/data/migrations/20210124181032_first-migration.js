da5e931bc61a315f7448d245e16de1a89bd800c9
JonathanMary <jon85ma@gmail.com>
2021-03-27 23:41:47 +0100

    migration + seed

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,13 +2,35 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
-      users.string('user_username', 200).notNullable()
+      users.string('user_username', 200).notNullable().unique()
       users.string('user_password', 200).notNullable()
       users.string('user_email', 320).notNullable()
       users.timestamps(false, true)
     })
+    .createTable('items', (users) => {
+      users.increments('item_id')
+      users.string('item_name', 200).notNullable()
+      users.boolean('item_available').defaultTo(false)
+      users.integer('user_id')
+           .references('user_id')
+           .inTable('users')
+           .notNullable()
+    })
+    .createTable('rent', (users) => {
+      users.increments('rent_id')
+      users.integer('item_id')
+           .references('item_id')
+           .inTable('items')
+           .notNullable()
+      users.integer('user_id')
+           .references('user_id')
+           .inTable('users')
+           .notNullable()
+    })
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('rent')
+  await knex.schema.dropTableIfExists('items')
   await knex.schema.dropTableIfExists('users')
 }
